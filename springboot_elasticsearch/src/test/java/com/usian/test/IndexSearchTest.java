31c6a741c4777725eb8c32ce21d02193f05cd03c
l326 <3458262441@qq.com>
2020-06-03 22:39:41 +0800

    提交elasticserch

---
+++ springboot_elasticsearch/src/test/java/com/usian/test/IndexSearchTest.java
@@ -0,0 +1,181 @@
+package com.usian.test;
+
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.text.Text;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.Operator;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class IndexSearchTest {
+
+    @Autowired
+    private RestHighLevelClient restHighLevelClient;
+
+    private SearchRequest searchRequest;
+    private SearchResponse searchResponse;
+
+    @Before
+    public void initSearchRequest() {
+        // 搜索请求对象
+        searchRequest = new SearchRequest("java1906");
+        searchRequest.types("course");
+    }
+
+    // 搜索type下的全部记录
+    @Test
+    public void testSearchAll() throws Exception {
+
+        // 搜索源构建对象
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+
+        // 设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        // 执行搜索
+        searchResponse = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+
+    }
+
+    @After
+    public void displayDoc() throws ParseException {
+        // 搜索匹配结果
+        SearchHits hits = searchResponse.getHits();
+        // 搜索总记录数
+        long totalHits = hits.totalHits;
+        System.out.println("共搜索到" + totalHits + "条文档");
+        // 匹配的文档
+        SearchHit[] searchHits = hits.getHits();
+        // 日期格式化对象
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        for (SearchHit hit : searchHits) {
+            // 文档id
+            String id = hit.getId();
+            System.out.println("id：" + id);
+            // 源文档内容
+            String source = hit.getSourceAsString();
+            System.out.println(source);
+        }
+    }
+
+    //分页查询
+    @Test
+    public void testSearchPage() throws Exception {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchSourceBuilder.from(1);
+        searchSourceBuilder.size(5);
+        searchSourceBuilder.sort("price", SortOrder.ASC);
+
+        // 设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        // 执行搜索
+        searchResponse = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+    }
+
+    @Test
+    public void testMatchQuery() throws Exception {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchQuery("name", "spring开发").operator(Operator.AND));
+                // 设置搜索源
+                searchRequest.source(searchSourceBuilder);
+        // 执行搜索
+        searchResponse = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+    }
+
+    @Test
+    public void testMultiMatchQuery() throws Exception {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.multiMatchQuery("开发","name","description"));
+
+        // 设置搜索源
+        searchRequest.source(searchSourceBuilder);
+        // 执行搜索
+        searchResponse = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+    }
+
+    @Test
+    public void testBooleanQuery() throws IOException {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //bool
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        //must
+        boolQueryBuilder.must(QueryBuilders.matchQuery("name", "开发"));
+        boolQueryBuilder.must(QueryBuilders.matchQuery("description","开发"));
+        searchSourceBuilder.query(boolQueryBuilder);
+        searchRequest.source(searchSourceBuilder);
+        searchResponse = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+    }
+
+    @Test
+    public void testFilterQuery() throws IOException {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        boolQueryBuilder.must(QueryBuilders.matchQuery("name","开发"));
+        boolQueryBuilder.filter(QueryBuilders.rangeQuery("price").gte(10).lte(100));
+        searchSourceBuilder.query(boolQueryBuilder);
+        searchRequest.source(searchSourceBuilder);
+        searchResponse = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+    }
+
+    @Test
+    public void testHighLightQuery() throws Exception {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchQuery("name", "spring"));
+        //设置高亮
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.preTags("<red>");
+        highlightBuilder.postTags("</red>");
+        highlightBuilder.fields().add(new HighlightBuilder.Field("name"));
+        searchSourceBuilder.highlighter(highlightBuilder);
+
+        searchRequest.source(searchSourceBuilder);
+        searchResponse = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+    }
+
+    @After
+    public void DisplayDoc() {
+        SearchHits searchHits = searchResponse.getHits();
+        long totalHits = searchHits.getTotalHits();
+        System.out.println("共搜索到" + totalHits + "条文档");
+
+        SearchHit[] hits = searchHits.getHits();
+        for (int i = 0; i < hits.length; i++) {
+            SearchHit hit = hits[i];
+            String id = hit.getId();
+            System.out.println("id：" + id);
+            String source = hit.getSourceAsString();
+            System.out.println(source);
+
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            if (highlightFields != null) {
+                HighlightField highlightField = highlightFields.get("name");
+                Text[] fragments = highlightField.getFragments();
+                System.out.println("高亮字段：" + fragments[0].toString());
+            }
+        }
+
+    }
+}
--- springboot_elasticsearch/src/test/java/com/usian/test/IndexWriterTest.java
+++ springboot_elasticsearch/src/test/java/com/usian/test/IndexWriterTest.java
@@ -3,10 +3,21 @@ package com.usian.test;
 import java.io.IOException;
 
 import com.usian.ElasticsearchApp;
+import org.elasticsearch.action.DocWriteResponse;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.IndicesClient;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -82,5 +93,69 @@ public class IndexWriterTest {
 		//得到响应结果
 		boolean acknowledged = deleteIndexResponse.isAcknowledged();
 		System.out.println(acknowledged);
+	}
+
+	//添加文档
+	@Test
+	public void testAddDocument() throws IOException {
+		//创建“索引请求”对象：索引当动词
+		IndexRequest indexRequest = new IndexRequest("java1906", "course", "2");
+		indexRequest.source("{\n" +
+				" \"name\":\"spring cloud实战\",\n" +
+				" \"description\":\"本课程主要从四个章节进行讲解： 1.微服务架构入门 " +
+				"2.spring cloud 基础入门 3.实战Spring Boot 4.注册中心eureka。\",\n" +
+				" \"studymodel\":\"201001\",\n" +
+				" \"price\":5.6\n" +
+				"}", XContentType.JSON);
+		IndexResponse indexResponse =
+				restHighLevelClient.index(indexRequest,RequestOptions.DEFAULT);
+		System.out.println(indexResponse.toString());
+	}
+
+	@Test
+	public void testBulkAddDocument() throws IOException {
+		BulkRequest bulkRequest = new BulkRequest();
+		bulkRequest.add(new IndexRequest("java1906", "course").source("{\"name\":\"php实战\",\"description\":\"php谁都不服\",\"studymodel\":\"201001\",\"price\":\"5.6\"}", XContentType.JSON));
+		bulkRequest.add(new IndexRequest("java1906", "course").source("{\"name\":\"net实战\",\"description\":\"net从入门到放弃\",\"studymodel\":\"201001\",\"price\":\"7.6\"}", XContentType.JSON));
+		BulkResponse bulkResponse = restHighLevelClient.bulk(bulkRequest,RequestOptions.DEFAULT);
+		System.out.println(bulkResponse.hasFailures());
+	}
+
+	//更新文档
+	@Test
+	public void testUpdateDocument() throws IOException {
+		UpdateRequest updateRequest = new UpdateRequest("java1906", "course", "1");
+		updateRequest.doc("{\n" +
+				" \"name\":\"spring cloud实战\",\n" +
+				" \"description\":\"本课程主要从四个章节进行讲解： 1.微服务架构入门 2.spring cloud 基础入门 3.实战Spring Boot 4.注册中心eureka。\",\n" +
+				" \"studymodel\":\"201001\",\n" +
+				" \"price\":5.5\n" +
+				"}", XContentType.JSON);
+		UpdateResponse updateResponse =
+				restHighLevelClient.update(updateRequest,RequestOptions.DEFAULT);
+		DocWriteResponse.Result result = updateResponse.getResult();
+		System.out.println(result);
+	}
+
+	//根据id删除文档
+	@Test
+	public void testDelDocument() throws IOException {
+		//删除索引请求对象
+		DeleteRequest deleteRequest = new DeleteRequest("java1906","course","1");
+		//响应对象
+		DeleteResponse deleteResponse =
+				restHighLevelClient.delete(deleteRequest,RequestOptions.DEFAULT);
+		System.out.println(deleteResponse.getResult());
+	}
+
+	//查询文档
+	@Test
+	public void getDoc() throws IOException {
+		GetRequest getRequest = new GetRequest("java1906","course","1");
+		GetResponse getResponse = restHighLevelClient.get(getRequest);
+		boolean exists = getResponse.isExists();
+		System.out.println(exists);
+		String source = getResponse.getSourceAsString();
+		System.out.println(source);
 	}
   }
